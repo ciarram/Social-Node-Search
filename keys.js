3c6616ff1d8ceaef4764e848d3086ec86331643a
Ciarra Murphy <mciarra@ymail.com>
2017-09-25 18:04:21 -0400

    linked keys to liri for twitter

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'process.env.TWITTER_dXjsw8ESZUpsYS1Do4fHrsuQm',
+  consumer_secret: 'process.env.TWITTER_CXSKjr1EgeslOTc00FV3NCOVwJzZVpuKv707ddWMiXKx7pFXRE',
+  access_token_key: 'process.env.TWITTER_912375616728653825-PBOTQ5BqDa2Olsd37O8fLj1NDygkGmc',
+  access_token_secret: 'process.env.TWITTER_YzwJYWfj175nG1yH4YT469QH0jBAwZ2vbz1CcItPvJDGm',
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,23 +1,28 @@
+var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var request = require("request");
 var movieParam = process.argv;
 var movieName = "";
+var tweets = require('./keys.js');
+console.log(tweets);
 
-var spotify = new Spotify({
-    id: '59a04cd1a6ea48f38896fec5af3160bc',
-    secret: '2f08c0e6e38f4f8dbfe411bd4a13ce9d'
-  });
-  
-  spotify.search({ type: 'track', query: 'Hello', limit: 1 }, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  
-  console.log(data.tracks.items[0].album.name); 
-  console.log(data.tracks.items[0].artists[0].name)
-  console.log(data.tracks.items[0].name)
 
-  });
+// var spotify = new Spotify({
+//     id: '59a04cd1a6ea48f38896fec5af3160bc',
+//     secret: '2f08c0e6e38f4f8dbfe411bd4a13ce9d'
+//   });
+  
+//   spotify.search({ type: 'track', query: 'Hello', limit: 1 }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     } else if (process.argv[2] == "spotify-this-song"){
+  
+//     console.log(data.tracks.items[0].album.name); 
+//     console.log(data.tracks.items[0].artists[0].name)
+//     console.log(data.tracks.items[0].name)
+//     console.log(data.tracks.items[0].external_urls)
+//     }
+//   });
 
 // for (var index = 2; index < movieParam.length; index++){
 //     if(index > 2 && index < movieParam.length){
