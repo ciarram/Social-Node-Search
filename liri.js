d67deba092da998b8a990a49aef2b182c6657488
Ciarra Murphy <mciarra@ymail.com>
2017-09-25 20:39:32 -0400

    added require to keys and fixed the twitterkeys to register on liri

--- liri.js
+++ liri.js
@@ -4,17 +4,23 @@ var request = require("request");
 var movieParam = process.argv;
 var movieName = "";
 
-var tweets = require('./keys.js');
+
+var twitterKeys = require('./keys.js');
 //console.log(tweets);
 var params = {screen_name: 'mciarra16'};
-twitterkeys.get('statuses/user_timeline', params, function(error, tweets, response){
+console.log(params);
+var myArgs = process.argv[2];
+console.log('myArgs: ', myArgs);
+if (myArgs == "my-tweets"){
+twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response){
         if (error) {
             console.log(error);
         } else{
-            console.log(tweets);
+            console.log(tweets.created_at);
+            console.log(tweets.text)
         }
     })
-
+}
 // var spotify = new Spotify({
 //     id: '59a04cd1a6ea48f38896fec5af3160bc',
 //     secret: '2f08c0e6e38f4f8dbfe411bd4a13ce9d'
@@ -32,31 +38,34 @@ twitterkeys.get('statuses/user_timeline', params, function(error, tweets, respon
 //     }
 //   });
 
-// for (var index = 2; index < movieParam.length; index++){
-//     if(index > 2 && index < movieParam.length){
-//         movieName = movieName + "+" + movieParam[index];
-//         console.log(movieName);
-//     } else{
-//         movieName += movieParam[index];
-//     }
-// }
+if(myArgs == "movie-this"){
 
-// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-// console.log(queryUrl);
-// request(queryUrl, function(error, response, body){
-//     if(error){
-//         throw error
-//     } else if(response.statusCode == 200 && "movie-this" == process.argv[2]){
-//         console.log("Title: " + JSON.parse(body).Title);
-//         console.log("Year Released: " +JSON.parse(body).Year);
-//         console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-//         console.log("Rotten Tomatoes Rating: " +JSON.parse(body).Ratings[1].Value);
-//         console.log("Produced In: " +JSON.parse(body).Country);
-//         console.log("Language of Movie: " +JSON.parse(body).Language);
-//         console.log("Plot: " +JSON.parse(body).Plot);
-//         console.log("Actors: " +JSON.parse(body).Actors);
-//     } //else if(!movieName){
+for (var index = 3; index < movieParam.length; index++){
+    if(index > 3 && index < movieParam.length){
+        movieName = movieName + "+" + movieParam[index];
+        console.log(movieName);
+    } else{
+        movieName += movieParam[index];
+    }
+}
 
-//     //}
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+console.log(queryUrl);
+request(queryUrl, function(error, response, body){
+    if(error){
+        throw error
+    } else if(response.statusCode == 200){
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Year Released: " +JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+        console.log("Rotten Tomatoes Rating: " +JSON.parse(body).Ratings[1].Value);
+        console.log("Produced In: " +JSON.parse(body).Country);
+        console.log("Language of Movie: " +JSON.parse(body).Language);
+        console.log("Plot: " +JSON.parse(body).Plot);
+        console.log("Actors: " +JSON.parse(body).Actors);
+    } //else if(!movieName){
 
-// })
+    //}
+
+})
+}
